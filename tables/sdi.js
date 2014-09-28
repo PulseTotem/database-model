8e2cdb309c33cc93acbc3f4f1939e5fdd7c92bcf
Simon Urli <simon.urli@gmail.com>
2014-09-28 17:09:33 +0200

    refs #2 Add a missing name attribute for SDI

--- tables/sdi.js
+++ tables/sdi.js
@@ -1,6 +1,7 @@
 var connection = require('./../database/connection.js');
 
 exports.schema = connection.sequelize.define('SDI', {
+	name: connection.Sequelize.STRING,
 	description: connection.Sequelize.STRING,
 	allowedHost: connection.Sequelize.STRING
 });
