a802eb48f3932c4e8b25860362680fee960eceb1
Christian Brel <ch.brel@gmail.com>
2015-04-03 18:37:16 +0200

    refs DATABASE-129 : Added complete attribute in OAuthKey model.

--- tables/oAuthKey.js
+++ tables/oAuthKey.js
@@ -3,5 +3,6 @@ var connection = require('./../database/connection.js');
 exports.schema = connection.sequelize.define('OAuthKey', {
     name: connection.Sequelize.STRING,
     description: connection.Sequelize.STRING,
-    value: connection.Sequelize.STRING
+    value: connection.Sequelize.STRING,
+    complete: connection.Sequelize.BOOLEAN
 });
