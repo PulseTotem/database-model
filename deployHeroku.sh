8460b00d735497b60ad330e653f991ed81459340
Christian Brel <ch.brel@gmail.com>
2015-04-24 11:16:28 +0200

    refs SERVICES-19 : Updated Heroku deployment.

--- deployHeroku.sh
+++ deployHeroku.sh
@@ -24,9 +24,10 @@ cd $current
 ### Copy : Database directory ###
 mkdir $herokuDir/database
 cp -r ./database/* $herokuDir/database
+rm $herokuDir/database/connection_infos.json
 ### Copy : Tables directory ###
 mkdir $herokuDir/tables
-cp -r ./database/* $herokuDir/tables
+cp -r ./tables/* $herokuDir/tables
 ### Copy : app.js & package.json files ###
 cp app.js $herokuDir
 cp package.json $herokuDir
@@ -44,3 +45,31 @@ git commit -m "v $commitMess"
 heroku config:set NPM_CONFIG_PRODUCTION=false
 git push heroku master
 heroku ps:scale web=1
+if [ "$2" == "true" ]
+then
+  echo "Waiting DB creation"
+  echo "0"
+  for i in {1..15}
+  do
+    echo "$i"
+    sleep 1s
+  done
+  echo "Re-deploy with 'drop=false' command."
+  heroku ps:scale web=0
+  cd $current
+  cp ProcfileDropFalse $herokuDir/Procfile
+  cd $herokuDir
+  commitMess=`date +"%D %T"`
+  git add .
+  git commit -m "v $commitMess"
+  heroku config:set NPM_CONFIG_PRODUCTION=false
+  git push heroku master
+  heroku ps:scale web=1
+fi
+
+#####
+#
+# Add database-dev add-on to dyno :
+# heroku addons:add heroku-postgresql:hobby-dev
+#
+#####
