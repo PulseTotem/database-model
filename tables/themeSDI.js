6da2fce1f07bf960ae3d86d1f76009bbc6cd0503
Simon Urli <simon.urli@gmail.com>
2015-05-28 10:51:33 +0200

    DATABASE-152 Rename attribute for defaultTheme

--- tables/themeSDI.js
+++ tables/themeSDI.js
@@ -4,7 +4,7 @@ exports.schema = connection.sequelize.define('ThemeSDI', {
 	name: connection.Sequelize.STRING,
 	description: connection.Sequelize.STRING,
 	complete: connection.Sequelize.BOOLEAN,
-	default: connection.Sequelize.BOOLEAN,
+	defaultTheme: connection.Sequelize.BOOLEAN,
 	background: connection.Sequelize.STRING(1024),
 	font: connection.Sequelize.STRING,
 	opacity: connection.Sequelize.STRING
