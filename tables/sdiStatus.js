916cd2d961e0f1f23ecd9f6de47193cbeddf644d
Simon Urli <simon.urli@gmail.com>
2015-06-08 08:12:38 +0200

    DATABASE-161 Update table SDIStatus to add new properties like name, online status and lastOnline date

--- tables/sdiStatus.js
+++ tables/sdiStatus.js
@@ -2,5 +2,8 @@ var connection = require('./../database/connection.js');
 
 exports.schema = connection.sequelize.define('SDIStatus', {
 	ip: connection.Sequelize.STRING,
+	name: connection.Sequelize.STRING,
+	online: connection.Sequelize.BOOLEAN,
+	lastOnline: connection.Sequelize.DATE,
 	complete: connection.Sequelize.BOOLEAN
 });
