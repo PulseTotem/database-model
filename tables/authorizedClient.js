b22def0cae9fb5b6bd90e155c719fb03a01cbba3
Simon Urli <simon.urli@gmail.com>
2015-06-08 14:36:45 +0200

    DATABASE-161 Rename table SDIStatus. And now consider authorizedClients separated with Client

--- tables/authorizedClient.js
+++ tables/authorizedClient.js
@@ -1,6 +1,6 @@
 var connection = require('./../database/connection.js');
 
-exports.schema = connection.sequelize.define('SDIStatus', {
+exports.schema = connection.sequelize.define('AuthorizedClient', {
 	ip: connection.Sequelize.STRING,
 	name: connection.Sequelize.STRING,
 	online: connection.Sequelize.BOOLEAN,
