dd49a7f751c09c44addf4a35080b7e2cdfa38989
Christian Brel <ch.brel@gmail.com>
2015-08-10 08:37:46 +0200

    refs DATABASE-164 : Change name to have a good one in AbsoluteEvent definition.

--- tables/absoluteEvent.js
+++ tables/absoluteEvent.js
@@ -1,6 +1,6 @@
 var connection = require('./../database/connection.js');
 
-exports.schema = connection.sequelize.define('AbsoluteTimeline', {
+exports.schema = connection.sequelize.define('AbsoluteEvent', {
 	name: connection.Sequelize.STRING,
 	begin: connection.Sequelize.DATE,
 	duration: {
